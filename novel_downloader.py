8637d3f908afd9234459a2479e931846c3af6968
Wayne Ding <wayne_ding@live.cn>
2024-03-23 15:42:31 +0800

    新增通过链接下载小说

--- novel_downloader.py
+++ novel_downloader.py
@@ -7,7 +7,7 @@ import click
 from playwright.sync_api import sync_playwright, expect as ep
 from playwright.sync_api._generated import Page
 
-
+_SOURCE_URL = "https://www.bbiquge.org/"
 _OUTPUT_DIR = "./novels"
 
 
@@ -40,19 +40,22 @@ class NovelDownloader:
         """
         return self.context.new_page()
 
-    def download_novel(self, novel_name: str) -> dict:
+    def download_novel(self, **kwargs) -> dict:
         """
         下载小说
-        :param novel_name: 小说名称
+        :param kwargs: 小说名称或链接
         :return:
         """
         result = dict()
-        search_result = self._search_novel(novel_name)
+        if "name" in kwargs:
+            search_result = self._search_novel(kwargs["name"])
+        else:
+            search_result = self._get_novel_info(kwargs["url"])
         if search_result["code"]:
             file_path = f"{_OUTPUT_DIR}/{search_result['name']}_{search_result['author']}.txt"
             file_handler = open(file_path, "a+", encoding="utf-8")
             current_chapter_index = 1
-
+            useless_str = f"笔趣阁 www.bbiquge.org，最快更新{search_result['name']} ！"
             is_went_wrong = False
             page = self.get_page()
             chapter_links = search_result["chapter_links"]
@@ -73,6 +76,7 @@ class NovelDownloader:
                         "\xa0", "").replace("\n\n", "\n").replace(useless_str, "").strip()
                     if len(chapter_title) * len(chapter_content) > 0:
                         file_handler.write(chapter_title)
+                        file_handler.write('\n')
                         file_handler.write(chapter_content)
                         file_handler.write('\n\n' + '-' * 10 + '\n\n')
                         current_chapter_index += 1
@@ -85,7 +89,7 @@ class NovelDownloader:
                         result["message"] = f"章节内容为空：{chapter_title}"
                 if is_went_wrong:
                     break
-                time.sleep(random.randint(1, 3))
+                # time.sleep(random.randint(0, 1))
             file_handler.close()
             if not is_went_wrong:
                 result["code"] = True
@@ -128,6 +132,7 @@ class NovelDownloader:
             page.goto(self.source_url)
         except TimeoutError:
             is_went_wrong = True
+            result["code"] = False
             result["message"] = "访问网站超时"
         if not is_went_wrong:
             search_input_locator = page.locator("//form/input[@name='searchkey']")
@@ -137,6 +142,7 @@ class NovelDownloader:
                 ep(search_submit_button_locator).to_be_visible()
             except AssertionError:
                 is_went_wrong = True
+                result["code"] = False
                 result["message"] = "未找到页面中的搜索栏"
             if not is_went_wrong:
                 search_input_locator.fill(novel_name)
@@ -180,14 +186,64 @@ class NovelDownloader:
         page.close()
         return result
 
+    def _get_novel_info(self, novel_url: str) -> dict:
+        search_popup_page = self.get_page()
+        is_went_wrong = False
+        result = dict()
+        try:
+            search_popup_page.goto(novel_url)
+        except TimeoutError:
+            is_went_wrong = True
+            result["code"] = False
+            result["message"] = "访问网站超时"
+        if not is_went_wrong:
+            novel_info_locator = search_popup_page.locator("#info")
+            try:
+                ep(novel_info_locator).to_be_visible()
+            except AssertionError:
+                is_went_wrong = True
+                result["code"] = False
+                result["message"] = "未找到小说信息"
+            if not is_went_wrong:
+                result["name"] = novel_info_locator.locator("h1").inner_text().strip()
+                result["link"] = search_popup_page.url
+                for novel_info_item in novel_info_locator.locator("p").all():
+                    item_text = novel_info_item.inner_text().strip()
+                    if item_text.startswith("作者"):
+                        result["author"] = item_text[item_text.find("：") + 1:]
+                    if item_text.startswith("最新"):
+                        result["latest_chapter"] = item_text[item_text.find("：") + 1:]
+                    if item_text.startswith("更新时间"):
+                        matched = re.search(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}).*共(\d+万)字", item_text)
+                        if matched:
+                            result["latest_updated_time"] = matched.group(1)
+                            result["word_count"] = matched.group(2)
+                chapter_links = list()
+                for chapter_link_locator in search_popup_page.locator(
+                        "//div[@id='list']/dl/center/following-sibling::dd/a").all():
+                    chapter_links.append(search_popup_page.url + chapter_link_locator.get_attribute("href"))
+                result["chapter_links"] = chapter_links
+                result["chapter_count"] = len(chapter_links)
+                result["code"] = True
+            search_popup_page.close()
+        return result
+
 
 @click.command()
-@click.argument("novel_name", type=str)
+@click.option("-n", "--name", type=str, help="小说名称")
+@click.option("-u", "--url", type=str, help="小说链接")
 @click.option("--headless", default=False, is_flag=True, help="是否显示浏览器")
 @click.option("-o", "--output_dir", default=_OUTPUT_DIR, help="输出目录")
-def cmd(novel_name, headless, output_dir):
+def cmd(name, url, headless, output_dir):
     output_dir = Path(output_dir)
     if not output_dir.exists():
         output_dir.mkdir(exist_ok=True, parents=True)
     downloader = NovelDownloader(headless, output_dir, _SOURCE_URL)
-    downloader.download_novel(novel_name)
+    if name is not None:
+        downloader.download_novel(name=name)
+    if url is not None:
+        downloader.download_novel(url=url)
+
+
+if __name__ == '__main__':
+    cmd()
